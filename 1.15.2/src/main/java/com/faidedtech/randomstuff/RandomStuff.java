4b056f52c2b49432a98b9fe8a4a327260e3ddf1a
David Adams <david@faidedtech.com>
2020-04-28 01:26:04 -0600

    Added Constructor
    Starting to build the mod itself

--- 1.15.2/src/main/java/com/faidedtech/randomstuff/RandomStuff.java
+++ 1.15.2/src/main/java/com/faidedtech/randomstuff/RandomStuff.java
@@ -1,5 +1,25 @@
 package com.faidedtech.randomstuff;
 
-public class RandomStuff {
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+/**
+ * RandomStuff: Main class for the Random Stuff Mod for Minecraft v. 1.15.2
+ * @author David Adams
+ *
+ */
+
+@Mod("randomstuff2")
+public final class RandomStuff {
+	
+	public static final String MODID = "randomStuff2";
+	public static final Logger LOGGER = LogManager.getLogger(MODID);
+	
+	/**
+	 * Default Constructor:
+	 */
+	public RandomStuff() {
+		
+	}
 }
